536e62c6cea3fc624af9b871cfed79a418c7d0e3
Simon <86124405@qq.com>
2017-06-20 15:54:50 +0800

    完成热门歌单歌曲列表页面

--- pages/taoge/taoge.js
+++ pages/taoge/taoge.js
@@ -6,78 +6,94 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    show: false,
+    marqueePace: 1,//滚动速度
+    marqueeDistance: 0,//初始滚动距离
+    marqueeDistance2: 0,
+    marquee2copy_status: false,
+    marquee2_margin: 60,
+    size: 14,
+    orientation: 'left',//滚动方向
+    interval: 20, // 时间间隔
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-    
+    this.onLoadData(options.id)
   },
-  onLoadData() {
-    wx.showLoading({
-      title: '加载中',
-      mask: true
-    })
-    app.api.getHotList().then(res => {
-      if(res.code === 0) {
+  onLoadData(id) {
+    wx.showNavigationBarLoading()
+    app.api.getHotList(id).then(res => {
+      if (res.code === 0) {
         this.setData({
-          
+          cdlist: res.cdlist[0]
+        })
+
+        app.api.getBcColor(res.cdlist[0].logo).then(res => {
+          if (res.code === 0) {
+            if(res.magic_color == 0){
+              res.magic_color = '000'
+            }
+            this.setData({
+              color: '#' + res.magic_color.toString(16),
+              show: true
+            })
+          }
         })
         setTimeout(() => {
-          wx.hideLoading()
-        }, 500)
+          wx.hideNavigationBarLoading()
+        }, 800)
       }
     })
-    
+
   },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+    
   },
-
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   }
 })
--- utils/api.js
+++ utils/api.js
@@ -1,4 +1,4 @@
-const url = 'https://c.y.qq.com/'
+const url = 'https://c.y.qq.com'
 const url2 = 'https://api.weixin.qq.com'
 const fetch = require('./fetch')
 
@@ -15,7 +15,7 @@ module.exports = {
    * 获取主页数据列表
    */
   getHomeList() {
-    return fetchApi('GET', `/musichall/fcgi-bin/fcg_yqqhomepagerecommend.fcg?format=json`).then(res => res.data)
+    return fetchApi('GET', `musichall/fcgi-bin/fcg_yqqhomepagerecommend.fcg?format=json`).then(res => res.data)
   },
   /**
    * 获取排行数据列表
@@ -27,7 +27,7 @@ module.exports = {
    * 获取搜索页面数据列表
    */
   getSearchList() {
-    return fetchApi('GET', `/splcloud/fcgi-bin/gethotkey.fcg?format=json`).then(res => res.data)
+    return fetchApi('GET', `splcloud/fcgi-bin/gethotkey.fcg?format=json`).then(res => res.data)
   },
   /**
    * 搜索关键词获取数据列表
@@ -39,12 +39,18 @@ module.exports = {
    * 获取选中的热门歌单的数据列表
    */
   getHotList(disstid) {
-    return fetchApi('GET', `/qzone/fcg-bin/fcg_ucc_getcdinfo_byids_cp.fcg?disstid=${disstid}&format=json`).then(res => res.data)
+    return fetchApi('GET', `qzone/fcg-bin/fcg_ucc_getcdinfo_byids_cp.fcg?disstid=${disstid}&format=json&type=1&utf8=1`).then(res => res.data)
   },
   /**
    * 获取登录凭证
    */
   getLoginCode(APPID, SECRET, JSCODE) {
-    return axios('GET', `sns/jscode2session?appid=${APPID}&secret=${SECRET}&js_code=${JSCODE}&grant_type=`)
-  }
+		return axios('GET', `sns/jscode2session?appid=${APPID}&secret=${SECRET}&js_code=${JSCODE}&grant_type=`).then(res => res.data)
+  },
+	/**
+	 * 获取背景色值
+	 */
+	getBcColor(img) {
+		return fetchApi('GET', `splcloud/fcgi-bin/fcg_gedanpic_magiccolor.fcg?pic_url=${img}&format=json`).then(res => res.data)
+	}
 }
